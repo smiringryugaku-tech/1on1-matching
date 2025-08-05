6def2a13c3514411a96ce7e3bb20e0b467730577
Shogo-Toiyama <shogo.toiyama@gmail.com>
2025-08-05 03:14:10 -0700

    Added a Supabase function for matching. Content is not added.

---
+++ supabase/functions/match_mentors/index.ts
@@ -0,0 +1,18 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+interface reqPayload {
+  name: string;
+}
+
+console.info('server started');
+
+Deno.serve(async (req: Request) => {
+  const { name }: reqPayload = await req.json();
+  const data = {
+    message: `Hello ${name}!`,
+  };
+
+  return new Response(
+    JSON.stringify(data),
+    { headers: { 'Content-Type': 'application/json', 'Connection': 'keep-alive' }}
+  );
+});
